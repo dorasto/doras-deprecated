a7bd8b4814f545ec6337f10a80f4e9909425c7cc
Tommerty <86270372+tommerty@users.noreply.github.com>
2023-11-07 16:29:11 +0000

    init

---
+++ tailwind.config.cjs
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx,astro}',
+    './components/**/*.{ts,tsx,astro}',
+    './app/**/*.{ts,tsx,astro}',
+    './src/**/*.{ts,tsx,astro}',
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+}
