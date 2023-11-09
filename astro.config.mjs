9241f0da0e851eea3351c9d997f78698befac34e
trent-gezel <trent.hopton@gmail.com>
2023-11-10 01:38:47 +1100

    trying to fix headers

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import cloudflare from "@astrojs/cloudflare";
 export default defineConfig({
 	integrations: [tailwind(), react()],
 	output: "server",
-	adapter: cloudflare()
+	adapter: cloudflare(),
 });
