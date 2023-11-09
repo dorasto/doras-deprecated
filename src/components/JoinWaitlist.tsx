232dffa01a3ed50e3d538a752f6748806891be98
trent-gezel <trent.hopton@gmail.com>
2023-11-10 02:50:04 +1100

    email integration

--- src/components/JoinWaitlist.tsx
+++ src/components/JoinWaitlist.tsx
@@ -1,8 +1,8 @@
+import React from 'react';
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-
 interface JoinWaitlistProps {
     text: string;
     variant?: "outline" | "ghost";
@@ -10,6 +10,19 @@ interface JoinWaitlistProps {
 }
 
 export function JoinWaitlist({ text, variant, className }: JoinWaitlistProps) {
+    const [Email, setEmail] = React.useState("");
+    async function FormSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        fetch(`/api/email/${Email}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then((res) => {
+                location.href = "/waitlist"
+            });
+    }
     return (
         <Dialog>
             <DialogTrigger asChild>
@@ -18,23 +31,25 @@ export function JoinWaitlist({ text, variant, className }: JoinWaitlistProps) {
                 </Button>
             </DialogTrigger>
             <DialogContent className=" bg-gradient-to-br from-rose-600 to-orange-400 text-white">
-                <DialogHeader>
-                    <DialogTitle className="text-2xl font-black">Join The Waitlist</DialogTitle>
-                    <DialogDescription className="text-white text-base">We're currently not ready yet, but you can sign up now to join the alpha when it launches! We'll just need some information first!</DialogDescription>
-                </DialogHeader>
-                <div className="space-y-4">
-                    <div>
-                        <Label htmlFor="email" className="text-right font-bold">
-                            Email
-                        </Label>
-                        <Input id="email" placeholder="your@email.com" className="col-span-3 bg-transparent font-bold placeholder:text-white text-white" required />
+                <form onSubmit={FormSubmit}>
+                    <DialogHeader>
+                        <DialogTitle className="text-2xl font-black">Join The Waitlist</DialogTitle>
+                        <DialogDescription className="text-white text-base">We're currently not ready yet, but you can sign up now to join the alpha when it launches! We'll just need some information first!</DialogDescription>
+                    </DialogHeader>
+                    <div className="space-y-4">
+                        <div>
+                            <Label htmlFor="email" className="text-right font-bold">
+                                Email
+                            </Label>
+                            <Input id="email" placeholder="your@email.com" className="col-span-3 bg-transparent font-bold placeholder:text-white text-white" onChange={e => setEmail(e.target.value)} required />
+                        </div>
                     </div>
-                </div>
-                <DialogFooter>
-                    <Button variant="outline" type="submit">
-                        Join Waitlist
-                    </Button>
-                </DialogFooter>
+                    <DialogFooter>
+                        <Button variant="outline" type="submit">
+                            Join Waitlist
+                        </Button>
+                    </DialogFooter>
+                </form>
             </DialogContent>
         </Dialog>
     );
