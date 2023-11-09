ef0400733b505e5a878281a7c44c410410276957
Tommy <86270372+tommerty@users.noreply.github.com>
2023-11-09 14:32:21 +0000

    fixing #2 where images weren't populating until hover when `-z-10` was set

--- src/components/BounceFeatures.tsx
+++ src/components/BounceFeatures.tsx
@@ -13,13 +13,13 @@ export const BouncyCardsFeatures = () => {
                             <CardTitle className="!text-left ">For the dreamers and the doers</CardTitle>
                             <CardDescription className="!text-left">Social management platforms should be for anyone, and we wanted to capture that! It's designed for content creators, artists, entrepreneurs, and for anybody who just likes to organize their things.</CardDescription>
                         </div>
-                        <div className="-z-10">
-                            <div className="absolute bottom-0 left-1/2 top-0 -translate-y-0 rounded-t-2xl bg-transparent p-4 transition-all duration-700 group-hover:-translate-y-10 group-hover:-rotate-6">
+                        <div className="hidden md:block">
+                            <div className="absolute  bottom-0 left-1/2 top-0 -translate-y-0 rounded-t-2xl bg-transparent p-4 transition-all duration-700 group-hover:-translate-y-10 group-hover:-rotate-6">
                                 <span className=" mx-auto text-center w-full font-semibold text-indigo-50">
                                     <img src="/blankbackdropyellowgreen.webp" className="w-3/4 group-hover:w-1/2 transition-all duration-700" />
                                 </span>
                             </div>
-                            <div className="absolute bottom-0 left-1/2 top-0 group-hover:-top-10 translate-y-2 rounded-t-2xl bg-transparent p-4 transition-all duration-700 group-hover:-translate-y-10 group-hover:-rotate-6">
+                            <div className="absolute  bottom-0 left-1/2 top-0 group-hover:-top-10 translate-y-2 rounded-t-2xl bg-transparent p-4 transition-all duration-700 group-hover:-translate-y-10 group-hover:-rotate-6">
                                 <span className=" mx-auto text-center w-full font-semibold text-indigo-50">
                                     <img src="/backdropitems1.webp" className="w-3/4 group-hover:w-1/2 transition-all duration-1000" />
                                 </span>
@@ -30,7 +30,7 @@ export const BouncyCardsFeatures = () => {
                 <BounceCard href="#features" className="col-span-12 md:col-span-4 row-span-2 bg-gradient-to-tl from-violet-400 via-pink-300 to-lime-300 shadow-lg">
                     <CardTitle>Link In Bio</CardTitle>
                     <CardDescription>Link in bio is a tool for content creators to share all of their links in one place</CardDescription>
-                    <div className="absolute bottom-0 left-4 right-4 top-20 -z-10 translate-y-8 scale-75 rounded-t-2xl bg-transparent p-4 transition-transform duration-700 group-hover:translate-y-4 group-hover:rotate-[2deg]">
+                    <div className="absolute  bottom-0 left-4 right-4 top-20  translate-y-8 scale-75 rounded-t-2xl bg-transparent p-4 transition-transform duration-700 group-hover:translate-y-4 group-hover:rotate-[2deg]">
                         <span className="block text-center font-semibold text-indigo-50">
                             <WindowMockup />
                         </span>
@@ -48,12 +48,12 @@ export const BouncyCardsFeatures = () => {
                 <BounceCard href="#features" className="col-span-12 md:col-span-4 bg-[radial-gradient(ellipse_at_bottom_left,_var(--tw-gradient-stops))] from-cyan-200 via-orange-200 to-yellow-300 shadow-lg">
                     <CardTitle>Link Shortener</CardTitle>
                     <CardDescription>Powerful link shortening tool to help your brand image</CardDescription>
-                    <div className="absolute bottom-0 left-4 right-4 top-28 translate-y-8 rounded-t-2xl bg-transparent p-4 transition-transform duration-500 group-hover:translate-y-4 group-hover:rotate-[2deg]">
+                    <div className="absolute  bottom-0 left-4 right-4 top-28 translate-y-8 rounded-t-2xl bg-transparent p-4 transition-transform duration-500 group-hover:translate-y-4 group-hover:rotate-[2deg]">
                         <span className="block text-center w-full font-semibold text-indigo-50">
                             <img src="/linkshortener.webp" />
                         </span>
                     </div>
-                    <div className="absolute bottom-0 left-4 right-4 top-14 translate-y-24 rounded-t-2xl bg-transparent p-4 transition-transform duration-700 group-hover:translate-y-2 group-hover:rotate-[2deg]">
+                    <div className="absolute  bottom-0 left-4 right-4 top-14 translate-y-24 rounded-t-2xl bg-transparent p-4 transition-transform duration-700 group-hover:translate-y-2 group-hover:rotate-[2deg]">
                         <span className="block text-center w-full font-semibold text-indigo-50">
                             <img src="/linkshortenerbutton.webp" />
                         </span>
@@ -61,13 +61,13 @@ export const BouncyCardsFeatures = () => {
                 </BounceCard>
                 <BounceCard href="#features" className="col-span-12 md:col-span-4 bg-gradient-to-r from-teal-200 to-lime-200 shadow-lg">
                     <CardTitle>Streamers</CardTitle>
-                    <CardDescription>Helping streamers showcase their livestreams, no matter the platform</CardDescription>
-                    <div className="absolute left-4 right-4 top-0 translate-y-2 rounded-t-2xl bg-transparent p-4 transition-transform duration-[250ms] group-hover:-translate-y-2 group-hover:rotate-[2deg] -z-10">
+                    <CardDescription className="z-20">Helping streamers showcase their livestreams, no matter the platform</CardDescription>
+                    <div className="absolute left-4 right-4 top-0 translate-y-2 rounded-t-2xl bg-transparent p-4 transition-transform duration-[250ms] group-hover:-translate-y-2 group-hover:rotate-[2deg] ">
                         <span className="block text-center w-full font-semibold text-indigo-50">
                             <img src="/livestreambackdrop.webp" />
                         </span>
                     </div>
-                    <div className="absolute bottom-0 left-4 right-4 top-14 translate-y-24 rounded-t-2xl bg-transparent p-4 transition-transform duration-700 group-hover:translate-y-16 group-hover:rotate-[2deg]">
+                    <div className="absolute  bottom-0 left-4 right-4 top-14 translate-y-24 rounded-t-2xl bg-transparent p-4 transition-transform duration-700 group-hover:translate-y-16 group-hover:rotate-[2deg]">
                         <span className="block text-center w-full font-semibold text-indigo-50">
                             <img src="/livenowasset.webp" />
                         </span>
@@ -78,7 +78,7 @@ export const BouncyCardsFeatures = () => {
                         <CardTitle className="">Media Kits</CardTitle>
                         <CardDescription>Easily customizable kit to allow you to share your stats with potential sponsors</CardDescription>
                     </div>
-                    <div className="absolute -z-10 bottom-0 left-4 right-4 top-16 translate-y-2 rounded-t-2xl bg-transparent p-4 transition-transform duration-[250ms] group-hover:-translate-y-2 group-hover:rotate-[-2deg]">
+                    <div className="absolute  bottom-0 left-4 right-4 top-16 translate-y-2 rounded-t-2xl bg-transparent p-4 transition-transform duration-[250ms] group-hover:-translate-y-2 group-hover:rotate-[-2deg]">
                         <span className="block text-center w-full font-semibold text-indigo-50">
                             <img src="/mediakitbackdrop.webp" />
                         </span>
@@ -88,7 +88,7 @@ export const BouncyCardsFeatures = () => {
                             <img src="/twittercard.webp" />
                         </span>
                     </div>
-                    <div className="absolute bottom-0 left-36 top-36 scale-75 translate-y-20 rounded-t-2xl bg-transparent p-4 transition-transform duration-700 group-hover:translate-y-2 group-hover:-rotate-6">
+                    <div className="absolute  bottom-0 left-36 top-36 scale-75 translate-y-20 rounded-t-2xl bg-transparent p-4 transition-transform duration-700 group-hover:translate-y-2 group-hover:-rotate-6">
                         <span className=" mx-auto text-center w-full font-semibold text-indigo-50">
                             <img src="/youtubecard.webp" />{" "}
                         </span>
@@ -102,16 +102,16 @@ export const BouncyCardsFeatures = () => {
 
 export const BounceCard = ({ className, children, href, whileHover = { scale: 0.95, rotate: "-1deg" } }: { className: string; href?: string; children: ReactNode; whileHover?: any }) => {
     return (
-        <motion.a href={href} whileHover={whileHover !== "false" ? whileHover : undefined} className={`group relative min-h-[300px] cursor-pointer overflow-hidden rounded-2xl bg-slate-100 p-8 ${className}`}>
+        <motion.a href={href} whileHover={whileHover !== "false" ? whileHover : undefined} className={`group relative min-h-[300px] cursor-pointer overflow-hidden rounded-2xl bg-slate-100 p-8 z-auto ${className}`}>
             {children}
         </motion.a>
     );
 };
 
 export const CardTitle = ({ children, className }: { children: ReactNode; className?: string }) => {
-    return <h3 className={`mx-auto text-center text-4xl font-black ${className}`}>{children}</h3>;
+    return <h3 className={`relative z-50 mx-auto text-center text-4xl font-black ${className}`}>{children}</h3>;
 };
 
 export const CardDescription = ({ children, className }: { children: ReactNode; className?: string }) => {
-    return <h3 className={` text-center text-xl font-semibold max-w-md ${className}`}>{children}</h3>;
+    return <h3 className={`relative text-center text-xl font-semibold max-w-md z-50 ${className}`}>{children}</h3>;
 };
