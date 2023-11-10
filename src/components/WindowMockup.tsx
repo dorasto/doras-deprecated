76b67b86624df450178822b2eb435ed49e888d90
Tommerty <86270372+tommerty@users.noreply.github.com>
2023-11-10 16:59:15 +0000

    updating link page style and iframe

--- src/components/WindowMockup.tsx
+++ src/components/WindowMockup.tsx
@@ -4,7 +4,7 @@ export function WindowMockup() {
     return (
         <div className="mx-auto border-surface-950 bg-transparent border-[10px] rounded-[2.5rem] h-[600px] w-[300px] overflow-y-auto ">
             {/* <LinksRender user={user} /> */}
-            <iframe className="w-full h-full" src="https://mystl.ink/mystlink"></iframe>
+            <iframe className="w-full h-full" src="https://doras.to/doras"></iframe>
         </div>
     );
 }
