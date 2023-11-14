ad6924baf03b75a2d3da9d100310df17a14632ed
trent-gezel <trent.hopton@gmail.com>
2023-11-15 02:55:39 +1100

    markdown stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,17 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import cloudflare from "@astrojs/cloudflare";
 
+import mdx from "@astrojs/mdx";
+import remarkToc from 'remark-toc';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), react()],
+	integrations: [tailwind(), react(), mdx()],
 	output: "server",
 	server: {
 		headers: {
 			"Access-Control-Allow-Origin": "*"
 		}
 	},
-	adapter: cloudflare(),
+	adapter: cloudflare()
 });
