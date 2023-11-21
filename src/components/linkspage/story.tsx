dde473f157dfc26c62d14393a8e4e865fd3baa86
trent-gezel <trent.hopton@gmail.com>
2023-11-22 00:22:46 +1100

    Update story.tsx

--- src/components/linkspage/story.tsx
+++ src/components/linkspage/story.tsx
@@ -1,3 +1,4 @@
+import { getTextColorForBackground } from "@/lib/utils";
 import type { ServerDataItem } from "@/server_types"; import Markdown from "react-markdown";
 ;
 
@@ -5,8 +6,11 @@ interface Props {
     user: ServerDataItem;
 }
 const StoryBlock: React.FC<Props> = ({ user }) => {
+    const textColorSolid = getTextColorForBackground(user.theme.background);
     return (
-        <Markdown className={""}>{user.story.text}</Markdown>
+        <div style={{ color: textColorSolid }}>
+            <Markdown className={""}>{user.story.text}</Markdown>
+        </div>
 
     );
 };
