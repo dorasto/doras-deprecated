82cc76ac1fad6469e4ae40657ed429c412ed319a
trent-gezel <trent.hopton@gmail.com>
2023-11-23 21:59:19 +1100

    blog and legal done for tommo

--- src/content/config.ts
+++ src/content/config.ts
@@ -18,8 +18,17 @@ const blogCollection = defineCollection({
   }),
 });
 
+const legalCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    date: z.string(),
+    updated: z.string(),
+  }),
+});
+
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
   'blog': blogCollection,
+  'legal': legalCollection
 };
