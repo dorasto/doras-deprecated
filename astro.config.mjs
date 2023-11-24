f85a3d81d2a98ba3f9b38a852939a6168b825eb3
trent-gezel <trent.hopton@gmail.com>
2023-11-24 16:21:44 +1100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
 	integrations: [tailwind({
 		applyBaseStyles: true
 	}), react(), mdx()],
-	output: "hybrid",
+	output: "server",
 	server: {
 		headers: {
 			"Access-Control-Allow-Origin": "*"
