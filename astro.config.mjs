d1741c09ed781c624f07c55e76202b7601efdb26
trent-gezel <trent.hopton@gmail.com>
2023-11-24 16:14:22 +1100

    cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import cloudflare from "@astrojs/cloudflare";
-import node from "@astrojs/node";
+// import node from "@astrojs/node";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
