4f606d050b8f8846ca521d670f23e6f12e3578fa
Tommerty <86270372+tommerty@users.noreply.github.com>
2023-11-25 19:48:48 +0000

    update roadmap

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -6,22 +6,13 @@ interface TimelineItem {
     title: string;
     details: string | React.ReactNode;
     buttontext?: string;
-    releaseNotes?: URL;
-    versionName: string;
+    releaseNotes?: string;
     progress?: "completed" | "upcoming";
 }
 
 interface Props {
     items: TimelineItem[];
 }
-
-const getVersion = (versionName: TimelineItem["versionName"]) => {
-    const versionTextMap: Record<TimelineItem["versionName"], string> = {
-        "0.1": "Closed Alpha",
-        "0.1.0": "Public Beta"
-    };
-    return versionTextMap[versionName];
-};
 
 const getStatusColor = (status: TimelineItem["progress"]) => {
     switch (status) {
@@ -46,57 +37,39 @@ const getStatusBgColor = (status: TimelineItem["progress"]) => {
 };
 
 const Timeline: React.FC<Props> = ({ items }) => {
-    const groupedItems = items.reduce(
-        (acc, item) => {
-            if (!acc[item.versionName]) {
-                acc[item.versionName] = [item];
-            } else {
-                acc[item.versionName].push(item);
-            }
-            return acc;
-        },
-        {} as Record<TimelineItem["versionName"], TimelineItem[]>
-    );
-
-    // Sort the items based on their progress status
-    const sortedItems = Object.values(groupedItems).map((items) =>
-        items.sort((a, b) => {
-            if (a.progress === "completed" && b.progress !== "completed") {
-                return -1;
-            } else if (a.progress !== "completed" && b.progress === "completed") {
-                return 1;
-            } else {
-                return 0;
-            }
-        })
-    );
+    const sortedItems = items.sort((a, b) => {
+        if (a.progress === "completed" && b.progress !== "completed") {
+            return -1;
+        } else if (a.progress !== "completed" && b.progress === "completed") {
+            return 1;
+        } else {
+            return 0;
+        }
+    });
 
     return (
         <div className="relative">
-            {sortedItems.map((items, index) => (
-                <div key={index} className={`border-s-0 h-auto space-x-3 ${getStatusColor(items[0].progress)}`}>
-                    <div className="p-3">
-                        <h2 className="mb-3 text-xl font-black leading-none text-surface-900 capitalize">{getVersion(items[0].versionName)}</h2>
-                        {items.map((item, index) => (
-                            <div
-                                key={index}
-                                className={`my-4 p-3 rounded-xl border-4 ${getStatusBgColor(item.progress)} shadow-xl text-surface-950`}
-                                style={{
-                                    borderColor: index < items.length - 1 ? getStatusColor(items[index + 1].progress) : getStatusColor(item.progress)
-                                }}
-                            >
-                                <div className="flex flex-col gap-2">
-                                    <h3 className="text-2xl font-black">{item.title}</h3>
-                                    <div className="font-bold">{item.details}</div>
-                                    {item.releaseNotes && (
-                                        <a href={item.releaseNotes.toString()} className="mt-3">
-                                            <Button className="w-fit">{item.buttontext}</Button>
-                                        </a>
-                                    )}
-                                    <p className="font-light text-sm mt-3">{item.date}</p>
-                                </div>
+            {sortedItems.map((item, index) => (
+                <div key={index} className={`border-s-0 h-auto space-x-3 ${getStatusColor(item.progress)}`}>
+                    <div className="p-2">
+                        <div
+                            key={index}
+                            className={`p-3 rounded-xl border-4 ${getStatusBgColor(item.progress)} shadow-xl text-surface-950`}
+                            style={{
+                                borderColor: index < sortedItems.length - 1 ? getStatusColor(sortedItems[index + 1].progress) : getStatusColor(item.progress)
+                            }}
+                        >
+                            <div className="flex flex-col">
+                                <h3 className="text-2xl font-black">{item.title}</h3>
+                                <div className="font-bold">{item.details}</div>
+                                {item.releaseNotes && (
+                                    <a href={item.releaseNotes.toString()} className="mt-3">
+                                        <Button className="w-fit">{item.buttontext}</Button>
+                                    </a>
+                                )}
+                                <p className="font-light text-sm mt-3">{item.date}</p>
                             </div>
-                        ))}
+                        </div>
                     </div>
                 </div>
             ))}
@@ -105,18 +78,10 @@ const Timeline: React.FC<Props> = ({ items }) => {
 };
 
 const timelineItems: TimelineItem[] = [
-    // {
-    //     date: "October 2023",
-    //     title: "First Launch",
-    //     details: "Built the initial development branch and first commits were made",
-    //     versionName: "0.1",
-    //     progress: "upcoming"
-    // },
     {
         date: "November 2023",
         title: "Waitlist Launch",
         details: "We launched our waitlist and started collecting emails for signups. Users who join the waitlist will be the first to know when we launch and will get closed alpha access in rolling stages. Not all users will be accepted into the closed alpha, and we will be prioritizing users who have been on the waitlist the longest",
-        versionName: "0.1",
         progress: "completed"
     },
     {
@@ -135,13 +100,20 @@ const timelineItems: TimelineItem[] = [
             </div>
         ),
         date: "November 24th, 2023",
-        versionName: "0.1",
-        progress: "completed"
+        progress: "completed",
+        buttontext: "Read the release notes",
+        releaseNotes: "https://doras.to/blog/alpha-launch"
+    },
+    {
+        date: "Early December 2023",
+        title: "Twitch Panel Integration",
+        details: "We're adding a feature under the \"Share\" options to generate an image based on your profile to add it to your Twitch panels. It's automatically sized, and is perfect for directing your viewers to your profile.",
+        progress: "upcoming"
     },
     {
         title: "Launch of Public Beta",
-        details: "",
-        versionName: "0.1.0"
+        details: "When we open the floodgates and let all users in!",
+        date: "December 2023"
     }
 ];
 
