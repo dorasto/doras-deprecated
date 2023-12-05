a5c06d86b4f4ee4cde8ea014fabb82137d27ca54
trent-gezel <trent.hopton@gmail.com>
2023-12-06 00:53:33 +1100

    Update posthog.tsx
    internal stuff

--- src/components/posthog.tsx
+++ src/components/posthog.tsx
@@ -1,5 +1,4 @@
 import posthog from "posthog-js";
 posthog.init("phc_rJeWBYHRnqdd66pXTrkRLNYf3IaNXue0MFICXekLztW", { api_host: "https://eu.posthog.com" });
-posthog.capture("Main Site", { property: "value" });
 export function PostHog({ }) {
 }
