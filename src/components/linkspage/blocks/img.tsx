bce9ff2e2aebf1d22459983e24ef1634adab2008
Tommerty <86270372+tommerty@users.noreply.github.com>
2023-12-11 20:18:12 +0000

    passing classname and adjusting style

--- src/components/linkspage/blocks/img.tsx
+++ src/components/linkspage/blocks/img.tsx
@@ -1,12 +1,11 @@
 import type { Links } from "@/server_types";
 
 interface Props {
-    link: Links
+    link: Links;
+    className?: string;
 }
-const ImgBlock: React.FC<Props> = ({ link }) => {
-    return (
-        <img src={link.url}></img>
-    )
-}
+const ImgBlock: React.FC<Props> = ({ link, className }) => {
+    return <img src={link.url} className={`rounded-xl shadow-xl mx-auto ${className}`} />;
+};
 
 export default ImgBlock;
