83e4702002e458e0193edf2fca01e24feb4dbadc
trent-gezel <trent.hopton@gmail.com>
2023-12-12 21:27:00 +1100

    added the widths
    https://github.com/doras-to/admin/issues/106

--- src/components/linkspage/blocks/img.tsx
+++ src/components/linkspage/blocks/img.tsx
@@ -2,10 +2,25 @@ import type { Links } from "@/server_types";
 
 interface Props {
     link: Links;
-    className?: string;
 }
-const ImgBlock: React.FC<Props> = ({ link, className }) => {
-    return <img src={link.url} className={`rounded-xl shadow-xl mx-auto ${className}`} />;
+const ImgBlock: React.FC<Props> = ({ link }) => {
+    let size = "w-full";
+    if (link.options?.size == 20) {
+        size = "w-1/6";
+    }
+    if (link.options?.size == 40) {
+        size = "w-2/6";
+    }
+    if (link.options?.size == 60) {
+        size = "w-4/6";
+    }
+    if (link.options?.size == 80) {
+        size = "w-5/6";
+    }
+    if (link.options?.size == 100) {
+        size = "w-full";
+    }
+    return <img src={link.url} className={`rounded-xl shadow-xl mx-auto ${size}`} />;
 };
 
 export default ImgBlock;
