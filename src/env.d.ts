c009c4d17a3fb7e1133fc28a372a7e6855380d4b
Tommerty <86270372+tommerty@users.noreply.github.com>
2023-12-29 15:24:09 +0000

    work on wordpress integration

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,5 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+interface ImportMetaEnv {
+    readonly STRAPI_URL: string;
+}
---
+++ src/interfaces/article.ts
@@ -0,0 +1,12 @@
+export default interface Article {
+    id: number;
+    attributes: {
+        title: string;
+        description: string;
+        content: string;
+        slug: string;
+        createdAt: string;
+        updatedAt: string;
+        publishedAt: string;
+    };
+}
